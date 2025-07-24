efd4eb0324ce5f06d30c3fd5f6925932e80e7816
Manuel Meraz <manuelmeraz2190@gmail.com>
2025-07-23 23:16:49 -0500

    fixing colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${geist.variable}`}>
-      <body>
+      <body className="bg-[#fbfbf3]"> {/* Set the background color here */}
         <TRPCReactProvider>
           <SiteNav />
           {children}
